12a2e8ea319efb4fa1f4dc43364e4a228a846017
Cleaver Barnes <295771+cleaver@users.noreply.github.com>
2025-03-17 17:52:19 -0400

    update content definition for custom id / slug

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,17 @@
 import { defineCollection } from 'astro:content';
 import { blogSchema } from './blog/_schema';
 import { aboutSchema } from './about/_schema';
-
+import path from 'path';
+import { glob } from 'astro/loaders';
 const blog = defineCollection({
-  type: 'content',
+  loader: glob({
+    pattern: '**/*.md',
+    base: './src/content/blog',
+    generateId: ({ entry }) => {
+      const fileName = path.basename(entry, path.extname(entry));
+      return `blog/${fileName}`;
+    }
+  }),
   schema: blogSchema
 });
 
